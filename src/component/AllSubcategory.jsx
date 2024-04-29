1e46ec7f38a6bd1e5e42fb5caaa8c9bc626cf593
Anik200338 <sarkaranik200338@gmail.com>
2024-04-29 11:53:21 +0600

    sucategory section design done

--- src/component/AllSubcategory.jsx
+++ src/component/AllSubcategory.jsx
@@ -7,18 +7,15 @@ const AllSubcategory = ({ Subcategory }) => {
   return (
     <Link
       to={`/Subcategory/${_id}`}
-      className="card card-compact lg:w-96 h-96 bg-base-100 shadow-xl"
+      className="card card-compact  h-[500px] bg-base-100 shadow-xl"
     >
-      <figure>
+      <figure className="h-40">
         <img src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title font-bold">{subcategory_Name}</h2>
-        <p className="text-lg ">{origins}</p>
+        <p className="text-sm ">{origins}</p>
         <p className="">{key_elements}</p>
-        <div className="card-actions justify-between">
-          <button className="btn">view</button>
-        </div>
       </div>
     </Link>
   );
